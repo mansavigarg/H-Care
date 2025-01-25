b93e551e552c0366bd3eafb2c6983e79dbf14b8d
Avi Maheshwari <avimaheshwari04@gmail.com>
2025-01-25 23:31:15 +0530

    add Navbar, HeroFooter

---
+++ Frontend/src/assets/images2/index.js
@@ -0,0 +1,7 @@
+import logo from "./logo.png"
+import CardsImage from "./cardsImage.jpg"
+
+export {
+    logo,
+    CardsImage,
+}
--- Frontend/src/pages/LandingPage/Landingpage.jsx
+++ Frontend/src/pages/LandingPage/Landingpage.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Navbar from './Navbar'
-import HeroMain from './HeroMain'
+import React from "react";
+import Navbar from "./Navbar";
+import HeroMain from "./HeroMain";
+import HeroFooter from "./HeroFooter";
+import Hero2 from "./Hero2";
 
 const Landingpage = () => {
   return (
     <div>
-        <Navbar />
-        <HeroMain />
+      <Navbar />
+      <HeroMain />
+      <HeroFooter />
+      <Hero2 />
     </div>
-  )
-}
+  );
+};
 
-export default Landingpage
+export default Landingpage;
